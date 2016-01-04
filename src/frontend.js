c1a43437d6fbf47afaac10eee65521507ebaa23e
toubi@ishtar <antoine@toubhans.org>
2016-01-04 17:20:20 +0100

    super js script added

--- src/frontend.js
+++ src/frontend.js
@@ -1,1 +1,15 @@
 // Antoine Toubhans FrontEnd js for dummy website
+
+var count_sec = 0
+
+var dumb_printer = function(){
+    // write something every 10 secondes
+    count_sec += 5;
+    x = document.createElement("div");
+    x.innerText = "Page loaded for "+count_sec+" sec...";
+    document.body.appendChild( x );
+    setTimeout("dumb_printer()", 5000);    
+}
+
+
+onload=function(){ dumb_printer() }
